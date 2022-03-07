61de04a8a2de3379552b69c76d9e7072d153eec1
swmao <WinsM@pku.edu.cn>
2022-03-07 16:29:57 +0800

    增加template中plot configuration

--- AllSample/template.py
+++ AllSample/template.py
@@ -7,6 +7,23 @@ from matplotlib import pyplot as plt
 
 sys.path.append("/mnt/c/Users/Winst/Nutstore/1/我的坚果云/XJIntern/PyCharmProject/")
 
+import warnings
+
+warnings.simplefilter("ignore")
+
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn
+
+seaborn.set_style("darkgrid")
+# plt.rc("figure", figsize=(16, 6))
+plt.rc("figure", figsize=(8, 3))
+plt.rc("savefig", dpi=90)
+# plt.rc("font", family="sans-serif")
+# plt.rc("font", size=12)
+plt.rc("font", size=10)
+
+plt.rcParams["date.autoformatter.hour"] = "%H:%M:%S"
 
 # %%
 if __name__ == '__main__':
