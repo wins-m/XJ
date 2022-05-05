4f38587dd29130a7ee53cbe1dc1dd5f12e1725f4
swmao <WinsM@pku.edu.cn>
2022-05-05 14:18:36 +0800

    optimize.py: TODO对CSI500和CSI300，不要不同的参数才能跑通

--- BarraPCA/optimize.py
+++ BarraPCA/optimize.py
@@ -31,15 +31,16 @@ conf = yaml.safe_load(open(conf_path, encoding='utf-8'))
 dat_path = conf['dat_path_barra']
 begin_date = '2016-02-01'
 end_date = '2022-03-31'
+mkt_type = 'CSI500'
 
 N_ingredient = 2000
-FL = -.1
-FH = .1
-HL = -.2
-HH = .2
-PL = -.1
-PH = .1
-D = 2
+FL = -.02
+FH = .02
+HL = -.04
+HH = .04
+PL = -.02
+PH = .02
+D = 1
 K = .01
 wei_tole = 1e-5
 
@@ -83,7 +84,6 @@ expo_indus: pd.DataFrame = expo[cols_indus]
 expo_style.tail()
 
 # Baseline Portfolio 股指成分
-mkt_type = 'CSI500'
 ind_cons = pd.read_csv(conf['idx_constituent'].format(mkt_type), index_col=0, parse_dates=True).reindex_like(dat).fillna(0)
 ind_cons = ind_cons / 100
 ind_cons.tail()
@@ -106,11 +106,12 @@ expo_pca = expo_pca.groupby(expo_pca.index.get_level_values(0)).apply(lambda s: 
 # %%
 portfolio_weight = pd.DataFrame()
 lst_w: pd.DataFrame = pd.DataFrame()
-w_lst = np.zeros([N_ingredient, 1])
+w_lst = None
 
-td0 = tradedates[-10]
+td0 = tradedates[0]
 for td0 in tradedates:
     td = td0.strftime('%Y-%m-%d')
+    print(td)
     # 个股覆盖
     stk_exposed_pca = set(expo_pca.loc[td].dropna(axis=1).index)  # PCA 暴露覆盖个股
     stk_exposed_barra = set(expo.loc[td].index)  # Barra 暴露 覆盖个股
@@ -119,11 +120,19 @@ for td0 in tradedates:
     stk_ind_cons = set(ind_cons.loc[td][ind_cons.loc[td] > 0].index)  # 指数成分股
     stk_alpha = set(dat.loc[td].dropna().index)  # Alpha 覆盖个股
 
-    tmp = stk_alpha.difference(stk_exposed)  # alpha 覆盖个股中barra暴露未知的
-    # stk_ingredient = set(dat.loc[td, list(stk_alpha)].rank(ascending=False).sort_values().index[:N_ingredient])  # 组合用股
-    stk_ingredient = set(dat.loc[td, list(stk_alpha.difference(tmp))].rank(ascending=False).sort_values().index[:N_ingredient])  # 组合用股
-    print(td + f'\tAlpha:{stk_alpha.__len__()}({tmp.__len__()})\t覆盖(暴露缺失)')
-    del tmp
+    # 全部alpha覆盖个股中选
+    N_ingredient = len(stk_alpha)
+    stk_ingredient = set(dat.loc[td, list(stk_alpha)].rank(ascending=False).sort_values().index[:N_ingredient])  # 组合用股
+    if w_lst is None:
+        w_lst = np.zeros([N_ingredient, 1])
+    # N_ingredient = 2000
+
+    # alpha中存在暴露的前N支个股中选
+    # tmp = stk_alpha.difference(stk_exposed)  # alpha 覆盖个股中barra暴露未知的
+    # print('指成分股缺暴露的', len(stk_ind_cons.difference(stk_alpha)))
+    # print(f' Alpha:{stk_alpha.__len__()}({tmp.__len__()})\t覆盖(暴露缺失)')
+    # stk_ingredient = set(dat.loc[td, list(stk_alpha.difference(tmp))].rank(ascending=False).sort_values().index[:N_ingredient])  # 组合用股
+    # del tmp
     complement_lst = set(lst_w.index).difference(stk_ingredient)  # 上期持有 组合资产未覆盖
     # complement_ind = stk_ind_cons.difference(stk_exposed)  # 指数成分股 组合资产未覆盖
 
@@ -139,32 +148,38 @@ for td0 in tradedates:
     p_del = expo_pca.loc[td].reindex_like(pd.DataFrame(index=list(wb.index), columns=expo_pca.columns)).fillna(0).T @ wb
     d_del = lst_w.loc[list(complement_lst)].abs().sum().values[0] if len(lst_w) > 0 else 0
 
-    fl = np.ones([len(cols_style), 1]) * FL - f_del.values.reshape(-1, 1)
-    fh = np.ones([len(cols_style), 1]) * FH - f_del.values.reshape(-1, 1)
-    hl = np.ones([len(cols_indus), 1]) * HL - h_del.values.reshape(-1, 1)
-    hh = np.ones([len(cols_indus), 1]) * HH - h_del.values.reshape(-1, 1)
-    pl = np.ones([expo_pca.shape[1], 1]) * PL - p_del.values.reshape(-1, 1)
-    ph = np.ones([expo_pca.shape[1], 1]) * PH - p_del.values.reshape(-1, 1)
+    fl = np.ones([len(cols_style), 1]) * FL + f_del.values.reshape(-1, 1)
+    fh = np.ones([len(cols_style), 1]) * FH + f_del.values.reshape(-1, 1)
+    hl = np.ones([len(cols_indus), 1]) * HL + h_del.values.reshape(-1, 1)
+    hh = np.ones([len(cols_indus), 1]) * HH + h_del.values.reshape(-1, 1)
+    pl = np.ones([expo_pca.shape[1], 1]) * PL + p_del.values.reshape(-1, 1)
+    ph = np.ones([expo_pca.shape[1], 1]) * PH + p_del.values.reshape(-1, 1)
     k = np.ones([N_ingredient, 1]) * K
     d = D - d_del
+
+    # len(stk_ind_cons.difference(stk_ingredient))
+    # w = wb.reindex_like(pd.Series(index=stk_ingredient)).fillna(0)
+    # w /= w.sum()
+    # w = w.values.reshape(-1, 1)
 
     w = cp.Variable((N_ingredient, 1), nonneg=True)
     objective = cp.Maximize(a.values.reshape(1, -1) @ w)
     constraints = [
-        fl <= xf.values.T @ w,
-        xf.values.T @ w <= fh,
-        # hl <= h.values.T @ w,
-        # h.values.T @ w <= hh,
-        pl <= p.values.T @ w,
-        p.values.T @ w <= ph,
-        w <= k, cp.sum(w) == 1
+        fl - xf.values.T @ w <= 0,
+        xf.values.T @ w - fh <= 0,
+        hl - h.values.T @ w <= 0,
+        h.values.T @ w <= hh,
+        pl - p.values.T @ w <= 0,
+        p.values.T @ w - ph <= 0,
+        w <= k,
+        cp.sum(w) == 1
     ]
     if len(lst_w) > 0:
         w_lst = lst_w.reindex_like(pd.DataFrame(index=list(stk_ingredient), columns=lst_w.columns)).fillna(0).values
         constraints.append(cp.norm(w - w_lst, 1) <= d)
 
     prob = cp.Problem(objective, constraints)
-    result = prob.solve(verbose=False)
+    result = prob.solve(verbose=True)
 
     if prob.status == 'optimal':
         w1 = w.value.copy()
@@ -179,7 +194,7 @@ for td0 in tradedates:
     portfolio_weight = pd.concat([portfolio_weight, lst_w.T])
 
 # %%
-portfolio_weight.to_csv(dat_path + 'portfolio_factor_apm_weekly.csv')
+portfolio_weight.to_csv(dat_path + f'portfolio_factor_apm_weekly[{mkt_type}].csv')
 portfolio_weight.sum(axis=1)
 (portfolio_weight > 0).sum(axis=1).plot()
 plt.show()
@@ -192,13 +207,14 @@ rtn_w2w = close_adj.pct_change(5).loc[tdays_d.loc[begin_date: end_date].index]
 rtn_portfolio = (portfolio_weight * rtn_w2w.reindex_like(portfolio_weight)).sum(axis=1)
 
 ind_cons_w = ind_cons.loc[tdays_d.loc[begin_date: end_date].index]
-rtn_csi500 = (ind_cons_w * rtn_w2w.reindex_like(ind_cons_w)).sum(axis=1)
+rtn_ind = (ind_cons_w * rtn_w2w.reindex_like(ind_cons_w)).sum(axis=1)
 
 df = pd.DataFrame()
 df['portfolio'] = rtn_portfolio
-df['csi500'] = rtn_csi500
+df[mkt_type] = rtn_ind
 tmp = df.cumsum().add(1)
 tmp.plot()
 plt.show()
-tmp.to_excel(dat_path + 'wealth_portfolio_factor_apm_weekly.xlsx')
+tmp.to_excel(dat_path + f'wealth_portfolio_factor_apm_weekly[{mkt_type}].xlsx')
 del tmp
+
